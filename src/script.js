bca0e41b3ccdd42b73bcb4964160a406452d842e
Arnab Das Utsa <144443522+iUtsa@users.noreply.github.com>
2025-03-18 23:18:32 -0400

    Delete src directory

--- src/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let headings = document.querySelectorAll(".page-heading");
-
-    headings.forEach(heading => {
-        if (document.title.includes(heading.textContent)) {
-            heading.classList.add("active-heading");
-        }
-    });
-});
